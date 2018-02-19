bdb99285b78217a406c48a2536e7632ba23aa609
paul <paul604@users.noreply.github.com>
2018-02-19 14:24:22 +0100

    init springBoot project

---
+++ back-end/src/main/java/fr/iut_nantes/quizomdb/QuizOmdbApplication.java
@@ -0,0 +1,12 @@
+package fr.iut_nantes.quizomdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizOmdbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuizOmdbApplication.class, args);
+    }
+}
