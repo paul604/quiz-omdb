17d96f8e987abecd0c1b6af870dc76d10d385a01
paul <paul604@users.noreply.github.com>
2018-02-21 11:35:43 +0100

    add javadoc, log and dep mongoDb

--- back-end/src/main/java/fr/iut_nantes/quizomdb/db/Idb.java
+++ back-end/src/main/java/fr/iut_nantes/quizomdb/db/Idb.java
@@ -5,32 +5,39 @@ import fr.iut_nantes.quizomdb.entite.Gamer;
 import java.util.List;
 
 /**
- *
+ * @version 1.0
+ * @since 1.0
  */
 public interface Idb {
 
     /**
      * @param pseudo name of gamer
-     * @param val number of answers
+     * @param val    number of answers
      * @return true if ok
+     * @since 1.0
      */
     boolean setAnswers(String pseudo, int val);
 
     /**
      * @param pseudo name of gamer
-     * @param val number of good answers
+     * @param val    number of good answers
      * @return true if ok
+     * @since 1.0
      */
     boolean setGoodAnswers(String pseudo, int val);
 
     /**
      * @param pseudo name of gamer
-     * @param pwd password
+     * @param pwd    password
+     * @return true if ok
+     * @since 1.0
      */
     boolean addGamer(String pseudo, String pwd);
 
     /**
      * @param gamer gamer
+     * @return true if ok
+     * @since 1.0
      */
     boolean addGamer(Gamer gamer);
 
@@ -38,26 +45,36 @@ public interface Idb {
     /**
      * @param pseudo name of gamer
      * @return number of answers
+     * @since 1.0
      */
     int getAnswers(String pseudo);
 
     /**
      * @param pseudo name of gamer
      * @return number of good answers
+     * @since 1.0
      */
     int getGoodAnswers(String pseudo);
 
     /**
      * @param pseudo name of gamer
      * @return Gamer if exist, null other
+     * @since 1.0
      */
     Gamer getGamer(String pseudo);
 
     /**
      * @return all gamer
+     * @since 1.0
      */
     List<Gamer> getAllGamer();
 
+    /**
+     * @param login name of gamer
+     * @param pwd   password of gamer
+     * @return true if login and pwd is good
+     * @since 1.0
+     */
     boolean connect(String login, String pwd);
 
 }
--- back-end/src/test/java/fr/iut_nantes/quizomdb/db/DbObjectTest.java
+++ back-end/src/test/java/fr/iut_nantes/quizomdb/db/DbObjectTest.java
@@ -4,27 +4,45 @@ import fr.iut_nantes.quizomdb.entite.Gamer;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 /**
- * Created by Paul on 20/02/18.
+ * @version 1.0
+ * @see fr.iut_nantes.quizomdb.db.DbObject
+ * @since 1.0
  */
 public class DbObjectTest {
 
-    Idb db;
+    private Idb db;
 
+    /**
+     * init db whit {@link DbObject}
+     *
+     * @throws Exception
+     * @since 1.0
+     */
     @Before
     public void setUp() throws Exception {
         db = new DbObject();
     }
 
+    /**
+     * test if after init, list of gamer is empty
+     *
+     * @throws Exception
+     * @since 1.0
+     */
     @Test
     public void listGamerIsEmpty() throws Exception {
         assertTrue("list Gamer Is not Empty", db.getAllGamer().isEmpty());
     }
 
+    /**
+     * add gamer whit {@link DbObject#addGamer(Gamer)}
+     *
+     * @throws Exception
+     * @since 1.0
+     */
     @Test
     public void addGamerDirectly() throws Exception {
         Gamer gamer = new Gamer("test", "pwdTest");
@@ -33,12 +51,18 @@ public class DbObjectTest {
         assertEquals("Object gamer is not equals", gamer, db.getGamer(gamer.getLogin()));
     }
 
+    /**
+     * add gamer whit {@link DbObject#addGamer(String, String)}
+     *
+     * @throws Exception
+     * @since 1.0
+     */
     @Test
     public void addGamerIndirectly() throws Exception {
         String name = "test";
         Gamer gamer = new Gamer(name, "pwdTest");
         db.addGamer(name, "pwdTest");
         assertFalse("List gamers is empty", db.getAllGamer().isEmpty());
-        assertEquals("Object gamer is not equals", gamer,  db.getGamer(name));
+        assertEquals("Object gamer is not equals", gamer, db.getGamer(name));
     }
 }
