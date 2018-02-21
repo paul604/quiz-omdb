c482fee72c5491c366f4cd728e93d1fe13928cf7
paul <paul604@users.noreply.github.com>
2018-02-21 15:42:24 +0100

    rename DataJwt to Constants :pencil2:

--- back-end/src/main/java/fr/iut_nantes/quizomdb/entite/Constants.java
+++ back-end/src/main/java/fr/iut_nantes/quizomdb/entite/Constants.java
@@ -3,7 +3,7 @@ package fr.iut_nantes.quizomdb.entite;
 import io.jsonwebtoken.impl.crypto.MacProvider;
 import java.security.Key;
 
-public class DataJwt {
+public class Constants {
 	
 	public static Key key = MacProvider.generateKey();	
 	
