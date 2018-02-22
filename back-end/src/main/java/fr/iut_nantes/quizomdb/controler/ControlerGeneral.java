e25ace0e7fe584dcae7aebefb622242a9d343b98
crowlas <22192006+crowlas@users.noreply.github.com>
2018-02-22 10:19:25 +0100

    Javadoc ControlerGeneral

--- back-end/src/main/java/fr/iut_nantes/quizomdb/controler/ControlerGeneral.java
+++ back-end/src/main/java/fr/iut_nantes/quizomdb/controler/ControlerGeneral.java
@@ -3,45 +3,90 @@ package fr.iut_nantes.quizomdb.controler;
 import fr.iut_nantes.quizomdb.entite.Constants;
 import io.jsonwebtoken.Jwts;
 
+/**
+ * @version 1.0
+ * @see fr.iut_nantes.quizomdb.controler.ControlerGeneral
+ * @since 1.0
+ */
 public class ControlerGeneral {
     private ControlerOMDB omdb;
     private ControlerGamer gamer;
 
 
+    /**
+     * @param token
+     * @return the actual question of the user
+     */
     public String getQuestion(String token) {
         return this.omdb.getQuestion(token);
     }
 
+    /**
+     * Change the actual question of a user
+     * @param token
+     * @return the new question
+     */
     public String generateQuestion(String token) {
         return this.omdb.generateQuestion(token);
     }
 
-    public boolean sendResponse(String token, String response) {
+    /**
+     * Check if the response in parameter is close to the answers
+     * @param token of the user
+     * @param response
+     * @return true of false
+     */
+    public boolean isCorrectResponse(String token, String response) {
         // TODO
         return false;
     }
 
+    /**
+     * Try to log in a user
+     * @param login
+     * @param password
+     * @return the token of the user if sucessful, else 'err'
+     */
     public String login(String login, String password) {
         try {
             return this.gamer.login(login, password);
         } catch (Exception e) {
-            return "Login or password invalid.";
+            return "err";
         }
     }
 
+    /**
+     * Delete the actual session of the user
+     * @param token of the user
+     */
     public void disconnect(String token) {
         this.gamer.disconnect(token);
     }
 
+    /**
+     *
+     * @param token of the user
+     * @return the number of good answers of the user
+     */
     public int getGoodAnswers(String token) {
         return this.gamer.getGamer(token).getGoodAnswers();
     }
 
+    /**
+     *
+     * @param token
+     * @return the number of answers of the user
+     */
     public int getAnswers(String token) {
         return this.gamer.getGamer(token).getAnswers();
     }
 
-    public boolean validateToken(String token) {
+    /**
+     * Check if the token is correct
+     * @param token of the user
+     * @return true if successful, else false
+     */
+    public boolean isCorrectToken(String token) {
         for (String login : this.gamer.getLogins()) {
             assert Jwts.parser().setSigningKey(Constants.key).parseClaimsJws(token).getBody().getSubject().equals(login);
             return true;
