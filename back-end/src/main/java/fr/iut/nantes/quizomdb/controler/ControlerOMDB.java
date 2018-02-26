d503fde02be13a72205ea9fe169c8c07d8357eb6
crowlas <22192006+crowlas@users.noreply.github.com>
2018-02-26 15:49:47 +0100

    Maj de la génération de question

--- back-end/src/main/java/fr/iut/nantes/quizomdb/controler/ControlerOMDB.java
+++ back-end/src/main/java/fr/iut/nantes/quizomdb/controler/ControlerOMDB.java
@@ -1,8 +1,16 @@
 package fr.iut.nantes.quizomdb.controler;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import fr.iut.nantes.quizomdb.entite.Quizz;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @version 1.0
@@ -21,21 +29,26 @@ public class ControlerOMDB {
      * @return the new question
      */
     public String generateQuestion(String login) {
-        String image , question , answers;
+        final GsonBuilder builder = new GsonBuilder();
+        final Gson gson = builder.create();
 
-        question = "What is the year of released of this movie ?";
+        String image , question , answers, film;
+
+        question = this.questionAlea() ;
+
+//        film = this.filmAlea();
+//        String filmjson = gson.toJson(film);
+
         image = "http://vignette2.wikia.nocookie.net/pingufan/images/2/2e/Pingu_the_movie_xxlg.png/revision/latest?cb=20170221164619";
         answers = "Pingu";
 
-
-        String json = "{ " +
-                "\"question\" : \""+ question +"\", " +
-                "\"image\" : \""+ image +
-                "\"}";
+        final Map<String, String> valeurs = new HashMap<String, String>();
+        valeurs.put("question", question);
+        valeurs.put("image", image);
 
 
-        this.addQuizz(login, json , answers);
-        return json;
+        this.addQuizz(login, gson.toJson(valeurs) , answers);
+        return gson.toJson(valeurs);
     }
 
     /**
@@ -78,6 +91,55 @@ public class ControlerOMDB {
         if (quizz != null) return quizz.getAnswers();
         return  null;
     }
+
+
+    /**
+     *
+     * @return a random question
+     */
+    private String questionAlea(){
+        return "What is the year of released of this movie ?";
+    }
+
+    /**
+     *
+     * @return a random title of film
+     */
+    private String filmAlea(){
+        String [] titles = new String[]{
+                "Pingu",
+        };
+        int nb = (int)(Math.random() * (titles.length));
+        String film= null;
+        try {
+            film = getHTML("http://www.omdbapi.com/?t="+titles[nb]);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return film;
+    }
+
+    /**
+     * do a HTTP GET
+     * @param urlToRead
+     * @return
+     * @throws Exception
+     */
+    private String getHTML(String urlToRead) throws Exception {
+        StringBuilder result = new StringBuilder();
+        URL url = new URL(urlToRead);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        String line;
+        while ((line = rd.readLine()) != null) {
+            result.append(line);
+        }
+        rd.close();
+        return result.toString();
+    }
+
+
 
 
 }
