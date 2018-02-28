9f6c0ca39d86177ba44b30fd9888cb82d4a249eb
paul <paul604@users.noreply.github.com>
2018-02-28 16:47:24 +0100

    war

--- back-end/src/main/java/fr/iut/nantes/quizomdb/application/QuizOmdbApplication.java
+++ back-end/src/main/java/fr/iut/nantes/quizomdb/application/QuizOmdbApplication.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,7 +22,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
  */
 @SpringBootApplication
 @Controller
-public class QuizOmdbApplication {
+public class QuizOmdbApplication extends SpringBootServletInitializer {
     ControlerGeneral control= new ControlerGeneral();
 
     /**
