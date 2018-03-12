8b0ee0bc0dd852dd05e7e9a6d461fa9be7ceae6a
crowlas <22192006+crowlas@users.noreply.github.com>
2018-03-12 11:52:10 +0100

    test token

--- back-end/src/test/java/fr/iut/nantes/quizomdb/controler/TestControlerGeneral.java
+++ back-end/src/test/java/fr/iut/nantes/quizomdb/controler/TestControlerGeneral.java
@@ -4,6 +4,7 @@ import fr.iut.nantes.quizomdb.Utils;
 import fr.iut.nantes.quizomdb.entite.Constants;
 import fr.iut.nantes.quizomdb.entite.Gamer;
 import io.jsonwebtoken.Jwts;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -14,14 +15,20 @@ import static org.junit.Assert.assertTrue;
 public class TestControlerGeneral {
     ControlerGeneral control;
     String token ;
+    ControlerGamer gamer;
 
     @Before
     public void initialize() {
         Utils.setupConfig();
         control = new ControlerGeneral();
-        ControlerGamer gamer = new ControlerGamer();
+        gamer = new ControlerGamer();
         gamer.getDb().addGamer(new Gamer("test",0,0), "test");
         token = gamer.addGamer("login", 0,0);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        gamer.getDb().supGamer("test");
     }
 
     @Test
@@ -31,8 +38,6 @@ public class TestControlerGeneral {
 
     @Test
     public void getLoginFromToken2(){
-        control.login("test", "test");
-        String token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0ZXN0In0.HPr7JiM6oXhk7s1xH_pTV5-yAuc41cXACldKTzBj66gEo7ArMKx3kCESEt7eWfXjiH3rEmjabLCMxS9_Cwm23Q";
         assertEquals("login",control.getLoginFromToken(token));
     }
 
